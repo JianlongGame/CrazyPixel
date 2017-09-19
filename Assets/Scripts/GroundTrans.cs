ab552544057f26363a88f0e0286042fbf5d40be0
JianlongGame <jianlonggamedesign@gmail.com>
2017-09-18 23:14:56 -0700

    Adding game world frame

---
+++ Assets/Scripts/GroundTrans.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTrans : MonoBehaviour {
+    public GameObject curGnd;
+    private float x;
+    private float y;
+
+	// Use this for initialization
+	void Start () {
+        curGnd.GetComponent<GameObject>();
+        x = curGnd.transform.position.x;
+        y = curGnd.transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (curGnd.transform.position.z <= -20f)
+        {
+            curGnd.transform.position = new Vector3(x, y, 40f);
+        }
+    }
+}
