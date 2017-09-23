fc8707ed913add4c3da60f6aad8f83f918b32270
SissiYu <>
2017-09-23 02:04:35 -0700

    complete obstacle generation

--- Assets/Scripts/GroundTrans.cs
+++ Assets/Scripts/GroundTrans.cs
@@ -7,11 +7,24 @@ public class GroundTrans : MonoBehaviour {
     private float x;
     private float y;
 
+	public List<GameObject> obstacles = new List<GameObject>();
+	public List<Transform> spawnPos = new List<Transform>();
+	public List<float> obstacleX = new List<float>();
+	public List<float> spawnX = new List<float>();
+	public int newPos, lastPos;
+
 	// Use this for initialization
 	void Start () {
         curGnd.GetComponent<GameObject>();
         x = curGnd.transform.position.x;
         y = curGnd.transform.position.y;
+
+		obstacleX.Add (-1.5f);
+		obstacleX.Add (1.5f);
+		obstacleX.Add (0f);
+		newPos = 0;
+		lastPos = 0;
+		setObstacle ();
     }
 	
 	// Update is called once per frame
@@ -19,6 +32,33 @@ public class GroundTrans : MonoBehaviour {
         if (curGnd.transform.position.z <= -20f)
         {
             curGnd.transform.position = new Vector3(x, y, 40f);
+			newPos += 1;
+
         }
+
+		if (lastPos != newPos)
+		{
+			lastPos = newPos;
+			setObstacle ();
+		}
     }
+
+	void setObstacle() {
+		foreach( Transform pos in spawnPos){
+			spawnX.Clear ();
+			int obsNumX = Random.Range (0, 2) + 1;
+			while (obsNumX > 0) {
+				float temp = obstacleX [Random.Range (0, 3)];
+				if (!spawnX.Contains(temp)){
+					spawnX.Add(temp);
+					obsNumX--;
+				}
+			}
+			foreach (float posX in spawnX) {
+				GameObject prefab = obstacles [Random.Range (0, 2)];
+				pos.position = new Vector3 (posX, pos.position.y, pos.position.z);
+				Instantiate (prefab, pos.position, prefab.transform.rotation);
+			}
+		}
+	}
 }
---
+++ Assets/Scripts/TimeDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDestroy : MonoBehaviour {
+
+	public float LifeTime = 10f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("DestroyObject", LifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void DestroyObject ()
+	{
+		Destroy (gameObject);
+	}
+}
