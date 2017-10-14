6fd9a6491940cff658190caa266a07af53c589f6
JianlongGame <jianlonggamedesign@gmail.com>
2017-10-14 01:07:46 -0700

    1. Add lane color change according to obstacle running on it 2. Add Collider box on each lane for later collision event implementation 3. Modifiy obstacles spawning mechnism 4. Modify textures(New textures)

---
+++ Assets/Scripts/OnCollisionColorChange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionColorChange : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject ground;
+    private Color orginColor;
+    public Material colorMaterial; 
+   
+	void Start () {
+		if (gameObject.name == "ColorSensor-1")
+        {
+            ground = GameObject.Find("Ground/Left");
+        }
+        else if (gameObject.name == "ColorSensor0")
+        {
+            ground = GameObject.Find("Ground/Middle");
+        }
+        else
+        {
+            ground = GameObject.Find("Ground/Right");
+        }
+        orginColor = colorMaterial.color;
+        ground.GetComponent<Renderer>().material = colorMaterial;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collide");
+        ground.GetComponent<Renderer>().material.color = other.gameObject.GetComponent<Renderer>().material.color;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        ground.GetComponent<Renderer>().material.color = orginColor;
+    }
+}
