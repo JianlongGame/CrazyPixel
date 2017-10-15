090d8648b06e20ad405f83443f036af29331a963
AloysiaWang <aloysia1225@outlook.com>
2017-10-15 01:05:43 -0700

    WanWang finish CountDown

---
+++ Assets/Scripts/SetCountDown.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCountDown : MonoBehaviour {
+	private GameController GC;
+
+	public void SetCountDownNow()
+	{
+		GC = GameObject.Find ("GameController").GetComponent<GameController> ();
+		GC.CounterDownDone = true;
+	}
+
+}
