b9b5b30d2febc83d497acc2aaaa84b86d198d221
AloysiaWang <aloysia1225@outlook.com>
2017-11-18 04:02:07 -0800

    changed the menu, add stages. Using the intger called stageNum

--- Assets/Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -5,7 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneOnClick : MonoBehaviour {
 
+	public static int stageNum;
+
 	public void LoadsByIndex(int sceneIndex){
 		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void StageNum(int stageIndex){
+		stageNum = stageIndex;
 	}
 }
---
+++ Assets/Scripts/StagesScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StagesScript : MonoBehaviour {
+	[SerializeField]
+	private Text valueText;
+
+	// Use this for initialization
+	void Start () {
+		if(LoadSceneOnClick.stageNum == 1)
+		{
+			valueText.text = "Stage  1";
+		}
+		if(LoadSceneOnClick.stageNum == 2)
+		{
+			valueText.text = "Stage  2";
+		}
+		if(LoadSceneOnClick.stageNum == 3)
+		{
+			valueText.text = "Stage  3";
+		}if(LoadSceneOnClick.stageNum == 4)
+		{
+			valueText.text = "Stage  4";
+		}
+		if(LoadSceneOnClick.stageNum == 5)
+		{
+			valueText.text = "Stage  5";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
