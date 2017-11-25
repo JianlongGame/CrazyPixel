fa7d1386da7eea69d53885614f4b651dd28c904e
JianlongGame <jianlonggamedesign@gmail.com>
2017-11-25 00:45:51 -0700

    Minor bug and merging fix

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,126 +1,4 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
-    public static Object thisLock = new Object();
-
-    [SerializeField] GameObject[] obstacles;
-    [SerializeField] DeathMenu deathMenu;
-	[SerializeField] PauseMenu pauseMenu;
-    [SerializeField] GameMenu gameMenu;
-    [SerializeField] AudioClip m_music;
-
-    public bool isGameOver;
-	public bool isGamePaused;
-    public bool isCountdownFinished;
-    public float speed = 0.001f;
-    public int stage;
-    public int lives;
-    public float time;
-    public int score;
-
-    [SerializeField] private Stat energy;
-    private const float coef = 2.0f;
-    AudioSource scoreMusic;
-
-    private void Awake()
-	{
-		energy.Initialize();
-	}
-
-    // Use this for initialization
-    void Start()
-    {
-        StartGame();
-        scoreMusic = GetComponent<AudioSource>();
-    }
-
-	void Update()
-	{
-		if (isGamePaused == false && isCountdownFinished == true && isGameOver == false) {
-			energy.CurrentVal -= coef * Time.deltaTime;
-            time += Time.deltaTime;
-            gameMenu.SetScore(score);
-            gameMenu.SetTime((int)time);
-        }
-    }
-
-    // Delete the previous game
-    void ClearGame()
-    {
-        foreach (Transform child in transform)
-        {
-            Destroy(child.gameObject);
-        }
-    }
-
-    // Start a new game
-    void StartGame()
-    {
-        ClearGame();
-        isGameOver = false;
-		isGamePaused = false;
-        isCountdownFinished = false;
-        Scrolling.gameOver = false;
-        GroundTrans.doSpawn = true;
-        lives = 2;
-        time = 0.0f;
-        score = 0;
-    }
-
-    public void loseOneLife()
-    {
-        if (lives >= 0)
-        {
-            gameMenu.RemoveLife(lives--);
-        }
-        if (lives < 0)
-        {
-            OnGameOver();
-        }   
-    }
-
-    public void winOneLife()
-    {
-        if (lives < 2)
-        {
-            gameMenu.AddLife(++lives);
-        }
-    }
-
-    public void rightShape()
-	{
-		//checkShape = true;
-		energy.CurrentVal += 10;
-	}
-
-    // Player has died
-    void OnGameOver()
-    {
-        GroundTrans.doSpawn = false;
-        Scrolling.gameOver = true;
-        isGameOver = true;
-        scoreMusic.clip = m_music;
-        scoreMusic.Play();
-        deathMenu.ToggleEndMenu(score);  //show the death button and pass the score when the player died
-    }
-   
-	public void PauseGame()
-	{
-		isGamePaused = true;
-		pauseMenu.TogglePauseMenu();
-        gameMenu.Pause(true);
-	}
-
-	public void ContinueGame()
-	{
-		isGamePaused = false;
-		pauseMenu.ClosePauseMenu();
-        gameMenu.Pause(false);
-	}
-}
-=======
 ﻿using UnityEngine;
 using UnityEngine.UI;
 public class GameController : MonoBehaviour
@@ -198,7 +76,7 @@ public class GameController : MonoBehaviour
         GroundTrans.doSpawn = true;
         lives = 2;
         time = 0.0f;
-        score = 28;
+        score = 0;
     }
 
     public void loseOneLife()
@@ -233,7 +111,7 @@ public class GameController : MonoBehaviour
         GroundTrans.doSpawn = false;
         Scrolling.gameOver = true;
         isGameOver = true;
-        deathMenu.ToggleEndMenu();  //show the death button when the player died
+        deathMenu.ToggleEndMenu(score);  //show the death button when the player died
     }
    
 	public void PauseGame()
@@ -250,4 +128,4 @@ public class GameController : MonoBehaviour
         gameMenu.Pause(false);
 	}
 }
->>>>>>> Stats update
+
